e903884fd0410e2cc1ffbe865733f58b45770e8a
Jeslienss <lijwnss@gmail.com>
2018-07-20 17:32:06 -0400

    ~

---
+++ README.md
@@ -0,0 +1,1 @@
+# XinyuInfocom18
---
+++ ethgas.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+import datetime
+import time
+from bs4 import BeautifulSoup
+
+counter = 0
+endCounter = 1    # record how many data
+interval = 20       # in seconds
+
+with open('result.csv', 'wb') as f:
+    while counter < endCounter:
+        driver = webdriver.Chrome()
+        driver.get('https://ethgasstation.info/')
+        pageSource = driver.page_source
+        pageSrc = repr(pageSource)
+        indexTemp = pageSrc.find('data: [', pageSrc.find('data: [', 0, len(pageSrc)) + 1, len(pageSrc))
+        indexStart = pageSrc.find('[', indexTemp, len(pageSrc))
+        indexEnd = pageSrc.find(']', indexStart, len(pageSrc))
+
+        soup = BeautifulSoup(pageSource, "lxml")
+        table = soup.find_all('table')[0]
+        tebleLen = (len(table.find_all('tr')))
+
+        tmpGasPrice = []
+        for row in table.findAll('tr')[1:tebleLen]:
+            col = row.findAll('td')
+            tmpGasPrice.append(col[1].getText())
+        tmpGasPrice = ','.join(tmpGasPrice)
+
+        now = datetime.datetime.now()
+        CurrentTime = str(now.hour) + '-' + str(now.minute) + '-' + str(now.second)
+        driver.quit()
+
+        counter = counter + 1
+        print(CurrentTime + ',' + pageSrc[indexStart + 1:indexEnd] + ',' + tmpGasPrice + ',\n')
+        f.write(CurrentTime + ',' + pageSrc[indexStart + 1:indexEnd] + ',' + tmpGasPrice + ',\n')
+        time.sleep(interval)
